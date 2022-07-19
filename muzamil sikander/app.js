39e319502565eeadb4a848555bc56bf5dee11208
muzamilsikander <102538533+Muzamil-Sikander@users.noreply.github.com>
2022-07-19 21:21:53 +0500

    Add files via upload

---
+++ muzamil sikander/app.js
@@ -0,0 +1,15 @@
+function beta(values){
+   var data = document.getElementById("inp-1").value += values;
+
+
+   var chhck = [ "ahmed", "raza"]
+
+function check(){
+    
+    
+    
+}     
+}
+
+
+ 
